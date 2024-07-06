b490c93908d4df61ea7b55aba33a1807e4ffbc08
Filipeelopess98 <filipee.lopess98@gmail.com>
2024-07-05 23:40:40 -0300

    commit 05/07

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,36 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'custom-blue': '#164686', 'custom-purple': '#221660', 'custom-black': '#010005',
+        'custom-blue': '#164686',
+        'custom-purple': '#160b3f',
+        'custom-black': '#010005',
       },
       boxShadow: {
         'custom-purple': '0 4px 6px rgba(108, 58, 224, 0.5)',
-
+      },
+      fontFamily: {
+        'great-vibes': ['"Great Vibes"', 'cursive'],
+        'bangers': ['"Bangers"', 'system-ui'],
+        'poppins': ['"Poppins"', 'sans-serif'],
+      },
+      fontSize: {
+        '10px': '10px',
+        '12px': '12px',
+        '14px': '14px',
+        '16px': '16px',
+        '18px': '18px',
+        '20px': '20px',
+        '24px': '24px',
+        '28px': '28px',
+        '32px': '32px',
+        '36px': '36px',
+        '40px': '40px',
+        '48px': '48px',
+        '56px': '56px',
+        '64px': '64px',
+        '72px': '72px',
       },
     },
   },
   plugins: [],
 }
-
-
-
